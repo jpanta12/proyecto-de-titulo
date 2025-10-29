225228546095986e331dbbe3db2048cd59df31fd
arielchacon20 <ariel.chacon@inacapmail.cl>
2025-10-28 23:01:43 -0300

    arreglo "layout.tsx"

--- printflow/src/app/layout.tsx
+++ printflow/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "app/components/Authcontext";
+import { AuthProvider } from "app/components/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
